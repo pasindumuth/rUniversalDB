669feeeb6fa3dc618f452cedd30ec0f35f85246f
Pasindu Muthukuda <pasindumuth@gmail.com>
2021-12-02 02:45:47 -0800

    More tests

--- src/bin/simtest/advanced_serial_test.rs
+++ src/bin/simtest/advanced_serial_test.rs
@@ -24,7 +24,46 @@ use std::collections::BTreeMap;
 // -----------------------------------------------------------------------------------------------
 
 pub fn test_all_advanced_serial() {
+  subquery_test();
   trans_table_test();
+}
+
+// -----------------------------------------------------------------------------------------------
+//  subquery_test
+// -----------------------------------------------------------------------------------------------
+
+fn subquery_test() {
+  let (mut sim, mut context) = setup();
+
+  // Setup Tables
+  setup_inventory_table(&mut sim, &mut context);
+  populate_inventory_table_basic(&mut sim, &mut context);
+  setup_user_table(&mut sim, &mut context);
+  populate_setup_user_table_basic(&mut sim, &mut context);
+
+  // Test Multiple Subqueries
+
+  {
+    let mut exp_result = TableView::new(vec![cno("product_id")]);
+    exp_result.add_row(vec![Some(cvi(0))]);
+    context.send_query(
+      &mut sim,
+      " SELECT product_id
+        FROM inventory AS inv
+        WHERE (
+          SELECT balance * 2
+          FROM user
+          WHERE email = inv.email) = 50 + (
+          SELECT balance
+          FROM user
+          WHERE email = inv.email) 
+      ",
+      100,
+      exp_result,
+    );
+  }
+
+  println!("Test 'subquery_test' Passed! Time taken: {:?}ms", sim.true_timestamp())
 }
 
 // -----------------------------------------------------------------------------------------------
@@ -59,6 +98,41 @@ fn trans_table_test() {
                    WHERE email = inventory.email))
         SELECT email
         FROM v2;
+      ",
+      100,
+      exp_result,
+    );
+  }
+
+  // Test TransTable Reads with a Write
+
+  {
+    let mut exp_result = TableView::new(vec![cno("product_id"), cno("count")]);
+    exp_result.add_row(vec![Some(cvi(1)), Some(cvi(30))]);
+    context.send_query(
+      &mut sim,
+      " UPDATE user
+        SET balance = balance + 20
+        WHERE email = (
+          WITH
+            v1 AS (SELECT email, balance
+                   FROM  user
+                   WHERE balance >= 60),
+            v2 AS (SELECT product_id, email
+                   FROM  inventory
+                   WHERE 0 < (
+                     SELECT COUNT(email)
+                     FROM v1
+                     WHERE email = inventory.email))
+          SELECT email
+          FROM v2);
+  
+        UPDATE inventory
+        SET count = count + 5
+        WHERE email = (
+          SELECT email
+          FROM user
+          WHERE balance >= 80);
       ",
       100,
       exp_result,
